c822b72df5b2789d96ddc0874a2ba28223b70a66
Aryan S. Jadon <95238912+Aryan-Jadon18@users.noreply.github.com>
2022-10-15 00:21:27 +0530

    new ques

---
+++ leetcode/medium/delte_middle_of_a_linked_list.cpp
@@ -0,0 +1,71 @@
+/*
+https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+Problems:
+You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+
+The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+
+For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ 
+
+Example 1:
+
+
+Input: head = [1,3,4,7,1,2,6]
+Output: [1,3,4,1,2,6]
+Explanation:
+The above figure represents the given linked list. The indices of the nodes are written below.
+Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+We return the new list after removing this node. 
+Example 2:
+
+
+Input: head = [1,2,3,4]
+Output: [1,2,4]
+Explanation:
+The above figure represents the given linked list.
+For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+Example 3:
+
+
+Input: head = [2,1]
+Output: [2]
+Explanation:
+The above figure represents the given linked list.
+For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+Node 0 with value 2 is the only node remaining after removing node 1.
+ 
+Solution:
+
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ 
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head -> next == nullptr)
+            return nullptr;
+        ListNode *p1=head, *p2=head;
+        //count number of nodes
+        int count=0;
+        while(p1 != nullptr){
+            count++;
+            p1=p1->next;
+        }
+        //reach middle node
+        int middleindex= count/2;
+        for(int i=0;i<middleindex-1;++i){
+            p2=p2->next;
+        }
+        //delete middle node and return head
+        p2->next=p2->next->next;
+        return head;
+    }
+};
+*/
